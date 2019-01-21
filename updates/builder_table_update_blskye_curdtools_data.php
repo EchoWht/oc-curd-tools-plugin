d7e3099643c135a16129872b05db8206af51b4f5
echowht <echowht@qq.com>
2019-01-21 22:25:28 +0800

    补充id自增

---
+++ updates/builder_table_update_blskye_curdtools_data.php
@@ -0,0 +1,23 @@
+<?php namespace Blskye\CurdTools\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateBlskyeCurdtoolsData extends Migration
+{
+    public function up()
+    {
+        Schema::table('blskye_curdtools_data', function($table)
+        {
+            $table->increments('id')->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('blskye_curdtools_data', function($table)
+        {
+            $table->integer('id')->change();
+        });
+    }
+}
